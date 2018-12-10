a05efe586445bb80e30ce02f61241da7e846a694
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2018-12-10 13:44:27 -0300

    Add sandbox global token; Remove URL from Order, using new constants file

--- wirecardpy/models/order.py
+++ wirecardpy/models/order.py
@@ -1,11 +1,12 @@
 from wirecardpy.utils import util
+from wirecardpy.utils import constants_sandbox
 
 
 class Order(object):
     """docstring for Order"""
     @staticmethod
     def create(ownId, items, customer):
-        url = 'https://sandbox.moip.com.br/v2/orders'
+        url = constants_sandbox.ORDER_URL if util.is_sandbox() else ''
         data = {
             'ownId': ownId,
             'items': items,
---
+++ wirecardpy/utils/constants_sandbox.py
@@ -0,0 +1,3 @@
+BASE_URL = 'https://sandbox.moip.com.br/v2/'
+
+ORDER_URL = BASE_URL + 'orders'
