8ce8860fd6217a911544a5cea51748d5185c2760
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2018-12-17 14:45:51 -0300

    Get accessToken and use it in post request

--- wirecardpy/utils/util.py
+++ wirecardpy/utils/util.py
@@ -18,13 +18,19 @@ def headers():
     return _headers
 
 
+def oauth2_headers(access_token):
+    _headers = {
+        'Authorization': 'Bearer ' + access_token,
+    }
+    return _headers
+
+
 def validate_response(response):
     if response.status_code == 200:
         return response.json()
     else:
         status_code = response.status_code
         response_json = response.json()
-        response_json['status_code'] = status_code
         error_message = 'WIRECARD REQUEST ERROR: Status ' + str(status_code) + \
                         'Request not sent. May contain errors as missing required parameters or transcription error. '
         raise RequestException(error_message, response_json)
@@ -32,7 +38,7 @@ def validate_response(response):
 
 def set_api_authorization(api_token, api_key, sandbox=False):
     global TOKEN
-    TOKEN['API_TOKEN'] = base64.b64encode('{}:{}'.format(api_token, api_key))
+    TOKEN['API_TOKEN'] = base64.b64encode('{}:{}'.format(api_token, api_key)).encode('utf-8')
     TOKEN['base_url'] = constants.BASE_URL_SANDBOX if sandbox else constants.BASE_URL_LIVE
 
 
@@ -44,5 +50,9 @@ def request_get(url, data={}):
     return validate_response(requests.get(url, params=data, headers=headers()))
 
 
-def request_post(url, data):
-    return validate_response(requests.post(url, json=data, headers=headers()))
+def request_post(url, data, access_token=None):
+    if access_token:
+        headers = oauth2_headers(access_token)
+    else:
+        headers = headers()
+    return validate_response(requests.post(url, json=data, headers=headers))
