c8bef80498bc31565a56093105d092e2fee1bfac
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2019-01-07 16:44:06 -0300

    Fix base URL usage

--- wirecardpy/models/balance.py
+++ wirecardpy/models/balance.py
@@ -5,4 +5,5 @@ from wirecardpy.utils import constants
 class Balance(object):
     @staticmethod
     def get_balance(access_token):
-        return util.request_get(constants.BALANCE_URL, access_token=access_token)
+        url = util.get_base_url() + constants.BALANCE_URL
+        return util.request_get(url, access_token=access_token)
